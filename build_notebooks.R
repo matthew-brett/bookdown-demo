11985f0e2fa8f22d08df07b516494b51a014e043
Matthew Brett <matthew.brett@gmail.com>
2019-11-27 00:43:02 +0000

    Use upstream output format for building
    See: https://github.com/rstudio/bookdown/pull/742

--- build_notebooks.R
+++ build_notebooks.R
@@ -1,26 +1,10 @@
 # Build Python notebook version
 # See: https://github.com/rstudio/bookdown/issues/782
-nb_document <- function () {
-    fmt <- rmarkdown::md_document(variant='gfm')
-    fmt$pandoc$ext = ".gfm"
-    args = c()
-    fig_caption = TRUE
-    md_extensions = NULL
-    number_sections = FALSE
-    from = rmarkdown::from_rmarkdown(fig_caption, md_extensions)
-    fmt$pre_processor = function(metadata, input_file, runtime, knit_meta,
-                                 files_dir, output_dir) {
-        bookdown:::process_markdown(input_file,
-                                    from,
-                                    args,
-                                    number_sections)
-        NULL
-    }
-    fmt
-}
-
 Sys.setenv(BOOK_ED='python-nb')
-fmt <- nb_document()
-bookdown::render_book('index.Rmd', fmt, clean=FALSE,
+# Markdown output
+fmt <- bookdown::markdown_document2(base_format=rmarkdown::md_document)
+# Turn off citation links.
+fmt$pandoc$args <- c(fmt$pandoc$args, '--metadata', 'link-citations=no')
+bookdown::render_book('index.Rmd', fmt,
                       config_file='_bookdown.yml',
                       output_dir='_notebooks')
