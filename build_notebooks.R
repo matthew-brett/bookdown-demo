3858c35d1db1746a33b8d42e9b79c3a779447af4
Matthew Brett <matthew.brett@gmail.com>
2019-11-27 00:41:57 +0000

    Final version of hackage for build_notebooks
    There's a real way to do it in bookdown now.

--- build_notebooks.R
+++ build_notebooks.R
@@ -1,7 +1,26 @@
 # Build Python notebook version
-fmt = rmarkdown::md_document(variant='gfm')
-fmt$pandoc$ext = ".gfm"
-fmt <- bookdown::epub_book()
-bookdown::render_book('index.Rmd', fmt,
+# See: https://github.com/rstudio/bookdown/issues/782
+nb_document <- function () {
+    fmt <- rmarkdown::md_document(variant='gfm')
+    fmt$pandoc$ext = ".gfm"
+    args = c()
+    fig_caption = TRUE
+    md_extensions = NULL
+    number_sections = FALSE
+    from = rmarkdown::from_rmarkdown(fig_caption, md_extensions)
+    fmt$pre_processor = function(metadata, input_file, runtime, knit_meta,
+                                 files_dir, output_dir) {
+        bookdown:::process_markdown(input_file,
+                                    from,
+                                    args,
+                                    number_sections)
+        NULL
+    }
+    fmt
+}
+
+Sys.setenv(BOOK_ED='python-nb')
+fmt <- nb_document()
+bookdown::render_book('index.Rmd', fmt, clean=FALSE,
                       config_file='_bookdown.yml',
                       output_dir='_notebooks')
